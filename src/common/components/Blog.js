9196d6e51af121b1d09028bb8c30e9043fa900bc
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2015-12-22 09:51:12 -0600

    Button component with Radium Styles

--- src/common/components/Blog.js
+++ src/common/components/Blog.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import Radium from 'radium';
 import Button from './buttons/Button.js';
 
-@Radium
 export default class Blog extends Component {
 
     render() {
         return (
             <div className="posts">
 
-                <Button size="sm" kind="primary">+ Article</Button>
+                <Button size="sm" kind="primary">Article</Button>
 
                 <h1 className="post-title">Blog</h1>
             </div>
